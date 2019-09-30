755ca2ede6e9b77d25c09298b84c8e874c2fd6b0
Liam Dennehy <liam@wiemax.net>
2019-09-30 20:10:47 +0200

    Helper: Persist TSP Services

--- app/Helpers.php
+++ app/Helpers.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use eIDASCertificate\Certificate\X509Certificate;
 
 abstract class Helpers
 {
@@ -49,5 +50,68 @@ abstract class Helpers
         }
         symlink('./tl-'.$tlURIId.'.xml',$symlink);
       }
+  }
+
+  public static function persistTSPService($tspServiceAttributes, $dataDir)
+  {
+    $tspServicesDir = $dataDir.'TSPServices/';
+    $certsDir = $dataDir.'certs/';
+    $skisDir = $dataDir.'SKIs/';
+    $casDir = $dataDir.'CAs/';
+    // Make sure we're processing associative arrays even if json is object-oriented
+    $tspServiceAttributes = json_decode(
+      json_encode($tspServiceAttributes),
+      true
+    );
+    $tspServiceId = hash('sha256',$tspServiceAttributes['name']);
+    $country = $tspServiceAttributes['trustServiceProvider']['trustedList']['schemeTerritory'];
+    $name = $country.': '.$tspServiceAttributes['name'];
+    $ski = $tspServiceAttributes['skiHex'];
+    if (! empty($ski)) {
+      $skiDir = $skisDir.$ski.'/';
+      if (!file_exists($skiDir)) {
+        mkdir($skiDir);
+      }
+    }
+    foreach ($tspServiceAttributes['certificates'] as $id => $PEM) {
+      $crt = new X509Certificate($PEM);
+      $crtId = $crt->getIdentifier();
+      file_put_contents($certsDir.'/'.$id.'.crt',$PEM);
+      if (! empty($ski)) {
+        $skiLinked = false;
+        $skiEntries = scandir($skiDir);
+        array_shift($skiEntries);
+        array_shift($skiEntries);
+        foreach ($skiEntries as $skiEntry) {
+          $parts = explode('/',readlink($skiDir.'/'.$skiEntry));
+          if (sizeof($parts) <> 4) {
+            var_dump(['type' => $tspServiceAttributes['type'],$ski, $crt->getSubjectKeyIdentifier(),$skiEntry,$parts]); exit;
+          }
+          $target = explode('/',readlink($skiDir.'/'.$skiEntry))[3];
+          if ($target == $crtId.'.crt') {
+            $skiLinked = true;
+          }
+        }
+        if (! $skiLinked) {
+          symlink('../../certs/'.$crtId.'.crt',$skiDir.$crtId.'.crt');
+        }
+      }
+      if ($crt->isCA()) {
+        if (file_exists($casDir.$crtId.'.crt')) {
+          unlink($casDir.$crtId.'.crt');
+        }
+        symlink('../certs/'.$crtId.'.crt',$casDir.$crtId.'.crt');
+      }
+      $crtTSPServiceJsonLink = $certsDir.$crtId.'.tspService.json';
+      if (file_exists($crtTSPServiceJsonLink)) {
+        unlink($crtTSPServiceJsonLink);
+      }
+      symlink('../TSPServices/'.$tspServiceId.'.json',$crtTSPServiceJsonLink);
+    }
+    file_put_contents(
+      $tspServicesDir.$tspServiceId.'.json',
+      json_encode($tspServiceAttributes,JSON_PRETTY_PRINT)
+    );
+
   }
 }
