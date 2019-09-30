f1ef7fe46e0476244fbb57d621b448d09cc567e9
Liam Dennehy <liam@wiemax.net>
2019-09-30 13:08:48 +0200

    TrustedLists: Caching helper

---
+++ app/Helpers.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App;
+
+
+abstract class Helpers
+{
+
+  public static function getTLXML($tlURI, $dataDir, $name)
+  {
+    $now = (int)date('U');
+    $tlXML = null;
+    $tlURIId = hash('sha256',$tlURI);
+    $tlDirPath = $dataDir.'TrustedLists/';
+    $tlFilePath = $tlDirPath.'/tl-'.$tlURIId.'.xml';
+    if (file_exists($tlFilePath)) {
+      $tlXMLAge = $now - filemtime($tlFilePath);
+      print "Cached '$name' is $tlXMLAge seconds old".PHP_EOL;
+      if ($tlXMLAge < 86400) {
+        print "Loading local '$name' from $tlURI".PHP_EOL;
+        $tlXML = file_get_contents($tlFilePath);
+      }
+    }
+    if (empty($tlXML)) {
+      print "Fetching $tlURI".PHP_EOL;
+      $tlXML = DataSource::getHTTP($tlURI);
+    }
+    return $tlXML;
+
+  }
+
+  public static function persistTLXML($tlURI, $dataDir, $tl)
+  {
+      $now = (int)date('U');
+      $tlXMLAge = null;
+      $tlName = $tl->getName();
+      $tlURIId = hash('sha256',$tlURI);
+      $tlDirPath = $dataDir.'TrustedLists/';
+      $tlFilePath = $tlDirPath.'/tl-'.$tlURIId.'.xml';
+      $symlink = $tlDirPath.$tlName.'.xml';
+      if (file_exists($tlFilePath)) {
+        $tlXMLAge = $now - filemtime($tlFilePath);
+      }
+      if (is_null($tlXMLAge) || $tlXMLAge >= 86400) {
+        print 'Saving TrustedList '.$tlName." in $tlFilePath".PHP_EOL;
+        file_put_contents($tlFilePath,$tl->getXML());
+        if (file_exists($symlink)) {
+          unlink($symlink);
+        }
+        symlink('./tl-'.$tlURIId.'.xml',$symlink);
+      }
+  }
+}
