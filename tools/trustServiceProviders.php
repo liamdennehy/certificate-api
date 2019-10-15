41d93c19910b771ba6e5dfe11574146a6b108c04
Liam Dennehy <liam@wiemax.net>
2019-10-15 16:57:52 +0200

    TSP export tool

---
+++ tools/trustServiceProviders.php
@@ -0,0 +1,119 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+use eIDASCertificate\Certificate\X509Certificate;
+use eIDASCertificate\TrustedList;
+use App\Http\Controllers\TrustedLists;
+use App\Http\Controllers\TrustServiceProviders;
+use App\Http\Controllers\TSPServices;
+use App\Http\Controllers\Certificates;
+use App\Helpers;
+
+$dotenv = Dotenv::create(__DIR__.'/../');
+$dotenv->load();
+if (empty(getenv('DATADIR'))) {
+  print "No DATADIR environment variable found.".PHP_EOL;
+  print "Set in current environment or place in the .env file.".PHP_EOL;
+  exit(1);
+}
+$dataDir = __DIR__ . '/../'.getenv('DATADIR').'/';
+$tlDir = $dataDir.'/TrustedLists/';
+if (!is_dir($dataDir)) {
+  print "DATADIR does not seem to exist.".PHP_EOL;
+  exit(1);
+}
+
+if (!array_key_exists(1,$argv)) {
+  usage($argv);
+  exit(1);
+} else {
+  $scheme = $argv[1];
+  switch ($scheme) {
+    case 'EU':
+      $lotlURI = TrustedList::ListOfTrustedListsXMLPath;
+      $lotlURIId = hash('sha256',$lotlURI);
+      break;
+    default:
+      print "Unrecognised trust scheme '$scheme'".PHP_EOL;
+      usage($argv);
+      exit(1);
+      break;
+  }
+}
+
+$lotlAttributes = json_decode(file_get_contents($tlDir.$lotlURIId.'.json'),true);
+$trustedListAttributes = [];
+$tlXML = [];
+$trustServiceProviders = new TrustServiceProviders($dataDir);
+$tspServices = new TSPServices($dataDir);
+// $trustedLists = new TrustedLists($dataDir);
+$trustedListDirFiles = array_diff(scandir($tlDir), array('..', '.'));
+foreach ($lotlAttributes['pointedTLs'] as $pointedTL) {
+  $tlId = $pointedTL['sourceId'];
+  $tlAttributes = json_decode(file_get_contents($tlDir.$tlId.'.json'),true);
+  $tlName = $tlAttributes['schemeTerritory'].': '.$tlAttributes['schemeOperatorName'];
+  print $tlName.' '.PHP_EOL;
+  if (!array_key_exists('tslSignatureVerifiedAt',$tlAttributes)) {
+    print $tlAttributes['schemeOperatorName'].' Not verified'.PHP_EOL;
+    continue;
+  } else {
+    $trustedListAttributes[$tlName] = $tlAttributes;
+    $tlTerritory = $tlAttributes['schemeTerritory'];
+    $tlXML[$tlName] = file_get_contents($tlDir.$tlId.'.xml');
+    try {
+
+      $tl = new TrustedList($tlXML[$tlName]);
+    } catch (\Exception $e) {
+      print $e->getMessage();
+      continue;
+    }
+
+    foreach ($tl->getTSPs() as $tsp) {
+      $tspName = $tsp->getName();
+      print '  '.$tspName.PHP_EOL;
+      $tspId = hash('sha256',$tlTerritory.': '.$tspName);
+      $tspAttributes = $tsp->getAttributes();
+      $trustServiceProviders->persistAttributes($tspAttributes);
+      foreach ($tsp->getTSPServices() as $tspService) {
+        $tspServices->persistAttributes($tspService->getAttributes());
+      }
+    }
+    // print memory_get_usage().PHP_EOL;
+  }
+}
+
+
+exit;
+$lotl = $trustedLists->getSigned($lotlURI, $signingCertDir, true); exit;
+
+
+foreach ($lotl['attributes']['pointedTLs'] as $name => $pointedTL) {
+  $tlName = $pointedTL['schemeTerritory'].': '.$pointedTL['schemeOperator'];
+  $tlXML = $trustedLists->getSourceXML($pointedTL['sourceURI']);
+  $tlName = $lotl['trustedList']->addTrustedListXML($tlName, $tlXML);
+  $tl = $lotl['trustedList']->getTrustedLists()[$tlName];
+  $tlSignedBy = $tl->getSignedBy();
+  if (empty($tlSignedBy)) {
+
+  } else {
+    $singedBy = $tl->getSignedBy();
+    $signedByHash = $singedBy->getIdentifier();
+    $certificates->persist($singedBy);
+    $tlSignedBy = $singedBy->getSubjectDN()."($signedByHash)";
+  }
+  $tlAttributes = $tl->getAttributes();
+  $tlAttributes['identifier'] = hash('sha256',$tl->getTSLLocation());
+  $trustedLists->persistAttributes($tlAttributes);
+  $trustedLists->persistXML($tl);
+
+  print 'Trusted List \''.$tl->getName().'\' Sequence Number '.$tl->getSequenceNumber().PHP_EOL;
+  print 'Signed By \''.$tlSignedBy.'\''.PHP_EOL;
+}
+function usage($argv)
+{
+  print "TrustedList Attribute exporter".PHP_EOL;
+  print "Usage:".PHP_EOL;
+  print $argv[0].' <scheme=EU> [trustedListName]'.PHP_EOL;
+}
